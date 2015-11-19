c2767893e1e7905b6fd5458be90f333374427df5
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-11-19 13:04:26 -0500

    dj - adding intellij 14.1.5 to the list of Casks

---
+++ Casks/intellij1415.rb
@@ -0,0 +1,17 @@
+cask :v1 => 'intellij1415' do
+  version '14.1.5'
+  sha256 'b253782bf1a10763c4fd84bffce0e28d855da8eb6499a91647860cb443695fdd'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version}-custom-jdk-bundled.dmg"
+  name 'IntelliJ IDEA'
+  homepage 'https://www.jetbrains.com/idea/'
+  license :commercial
+
+  app 'IntelliJ IDEA 14.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/IntelliJIdea14',
+                  '~/Library/Preferences/IntelliJIdea14',
+                  '~/Library/Preferences/com.jetbrains.intellij.plist',
+                 ]
+end
