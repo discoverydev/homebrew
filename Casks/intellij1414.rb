b819346d3058a7492781ca1d6c64748cae806c8f
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-11-19 12:59:06 -0500

    dj - rename

--- Casks/intellij1414.rb
+++ Casks/intellij1414.rb
@@ -1,15 +1,3 @@
-cask :v1 => 'intellij14_1_4' do
-  version '14.1.4'
-  sha256 '74fbb87e9459afe882199242264c23bf827bacccc4a6cafad966261203f0296f'
-
-  # bitbucket.org is the official download host per the vendor homepage
-  url "https://bitbucket.org/appium/appium.app/downloads/appium-#{version}.dmg"
-  name 'Appium'
-  homepage 'http://appium.io'
-  license :apache
-
-  app 'Appium.app'
-end
 cask :v1 => 'intellij-idea' do
   version '14.1.4'
   sha256 'b253782bf1a10763c4fd84bffce0e28d855da8eb6499a91647860cb443695fdd'
