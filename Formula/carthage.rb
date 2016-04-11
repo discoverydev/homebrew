18026bc3168d18ef953c642e872a701b23aa5e84
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2016-04-11 14:39:54 -0400

    Coleman, Hans - maybe fixing this formula?

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -4,13 +4,13 @@ class Carthage < Formula
   url "https://github.com/Carthage/Carthage.git", :tag => "0.15.2",
                                                   :revision => "f811a275aaa06df53119bbfad42af754fb7b6ff7",
                                                   :shallow => false
-  head "https://github.com/Carthage/Carthage.git", :shallow => false
+#  head "https://github.com/Carthage/Carthage.git", :shallow => false
 
-  bottle do
-    cellar :any
-    sha256 "f0d14960621fc28712fbee016ddc7851adf72f06ff8ce61919cdea987c85e2f7" => :el_capitan
-    sha256 "2b998dc2b8c5913d579b0a134be93e19b37a16d410463fc0c7168d358c8fe18d" => :yosemite
-  end
+#  bottle do
+#    cellar :any
+#    sha256 "f0d14960621fc28712fbee016ddc7851adf72f06ff8ce61919cdea987c85e2f7" => :el_capitan
+#    sha256 "2b998dc2b8c5913d579b0a134be93e19b37a16d410463fc0c7168d358c8fe18d" => :yosemite
+#  end
 
   depends_on :xcode => ["7.1", :build]
 
